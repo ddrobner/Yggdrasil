3131dd1067edb81bb37e04ebfee9de9243ebc8eb
Andrew Azores <andrew.azores@gmail.com>
2014-12-25 23:03:34 -0500

    Generate README.html into doc subdir

--- generate-docs.sh
+++ generate-docs.sh
@@ -6,4 +6,4 @@ command -v javadoc >/dev/null 2>&1|| { echo "javadoc is required but not found."
 javadoc -d doc/javadoc -sourcepath src -subpackages ca.team3161 -source 1.4
 
 command -v asciidoctor >/dev/null 2>&1 || { echo "asciidoctor is required but not found."; exit 1; }
-asciidoctor README.asciidoc
+asciidoctor README.asciidoc -D doc
