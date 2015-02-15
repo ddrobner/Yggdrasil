157ae0e73b0ba49fc6f4913c164838831cc40dd4
Andrew Azores <me@andrewazor.es>
2015-02-15 16:37:30 -0500

    Add EncoderRatePidSrc

---
+++ src/ca/team3161/lib/robot/pid/EncoderRatePidSrc.java
@@ -0,0 +1,64 @@
+/* Copyright (c) 2014, FRC3161
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+* 
+* * Redistributions in binary form must reproduce the above copyright notice, this
+*   list of conditions and the following disclaimer in the documentation and/or
+*   other materials provided with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package ca.team3161.lib.robot.pid;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+import java.util.Objects;
+
+/**
+ * A PID source that returns values as encoder rate.
+ */
+public final class EncoderRatePidSrc implements PIDSrc<Encoder> {
+
+    private final Encoder enc;
+
+    /**
+     * Create a new EncoderRatePidSrc instance.
+     * @param enc an Encoder object to use as a PIDSrc
+     */
+    public EncoderRatePidSrc(final Encoder enc) {
+        Objects.requireNonNull(enc);
+        this.enc = enc;
+    }
+
+    /**
+     * Retrieve the original sensor used to construct this PIDSrc.
+     * @return the Encoder
+     */
+    @Override
+    public Encoder getSensor() {
+        return enc;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public float getValue() {
+        return (float)enc.getRate();
+    }
+}
