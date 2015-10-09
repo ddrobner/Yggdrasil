d2d280d319cd0462f1fb36ae4304ce8717332f57
Andrew Azores <me@andrewazor.es>
2015-10-09 01:46:01 -0400

    Download checkstyle automatically (if required) on ant init

--- checkstyle/download.sh
+++ checkstyle/download.sh
@@ -13,15 +13,22 @@ else
     VERSION="$1"
 fi
 
+DOWNLOAD_LOCATION="$(dirname $(readlink -f "$0"))"
 FILENAME="checkstyle-$VERSION-all.jar"
 USER_AGENT="Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0"
 DOWNLOAD_URL="http://sourceforge.net/projects/checkstyle/files/checkstyle/$VERSION/checkstyle-$VERSION-all.jar/download"
 
-echo "Will download $DOWNLOAD_URL to $(pwd)/$FILENAME. Continue?"
+if [ -e "$DOWNLOAD_LOCATION/$FILENAME" ]; then
+    echo "$DOWNLOAD_LOCATION/$FILENAME already exists. Aborting."
+    exit 0
+fi
+
+echo "Will download $DOWNLOAD_URL to $DOWNLOAD_LOCATION/$FILENAME. Continue?"
 
 select yn in "Yes" "No"; do
     case $yn in
-        Yes) curl -L "$DOWNLOAD_URL" -o "$FILENAME"
+        Yes) mkdir -p "$DOWNLOAD_LOCATION"
+            curl -L "$DOWNLOAD_URL" -o "$DOWNLOAD_LOCATION/$FILENAME"
             break
             ;;
         No) exit 1;;
