2901cc90e553b75f0e0cb9d81431716e9a8fc1e4
Andrew Azores <me@andrewazor.es>
2015-10-09 01:24:35 -0400

    Fixed checkstyle download script to not use hardcoded URL

--- checkstyle/download.sh
+++ checkstyle/download.sh
@@ -15,13 +15,13 @@ fi
 
 FILENAME="checkstyle-$VERSION-all.jar"
 USER_AGENT="Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0"
-CHECKSTYLE_MAVEN_URL="http://downloads.sourceforge.net/project/checkstyle/checkstyle/6.11.2/checkstyle-6.11.2-all.jar?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fcheckstyle%2Ffiles%2Fcheckstyle%2F6.11.2%2F&ts=1444367565&use_mirror=iweb"
+DOWNLOAD_URL="http://sourceforge.net/projects/checkstyle/files/checkstyle/$VERSION/checkstyle-$VERSION-all.jar/download"
 
-echo "Will download $CHECKSTYLE_MAVEN_URL to $(pwd)/$FILENAME. Continue?"
+echo "Will download $DOWNLOAD_URL to $(pwd)/$FILENAME. Continue?"
 
 select yn in "Yes" "No"; do
     case $yn in
-        Yes) wget --user-agent="$USER_AGENT" --output-document="$FILENAME" "$CHECKSTYLE_MAVEN_URL"
+        Yes) curl -L "$DOWNLOAD_URL" -o "$FILENAME"
             break
             ;;
         No) exit 1;;
