2929a05a8df7dbe654d83a81d6af7a24940233e4
Andrew Azores <me@andrewazor.es>
2016-10-01 14:30:44 -0400

    Add TractionController

---
+++ src/ca/team3161/lib/robot/pid/TractionController.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2016, FRC3161.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ca.team3161.lib.robot.pid;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ * A VelocityController with toggleable "traction control". When disabled, the PID control is bypassed and the input
+ * values are fed directly into the backing SpeedController instance.
+ */
+public class TractionController extends VelocityController {
+
+    private boolean tractionEnabled = false;
+
+    public TractionController(final SpeedController speedController, final Encoder encoder, final float maxRotationalRate, final float kP, final float kI, final float kD, final float maxIntegralError, final float deadband) {
+        super(speedController, encoder, maxRotationalRate, kP, kI, kD, maxIntegralError, deadband);
+    }
+
+    public TractionController(final SpeedController speedController, final PIDRateValueSrc<Encoder> encoderPidSrc, final float maxRotationalRate, final float kP, final float kI, final float kD, final float maxIntegralError, final float deadband) {
+        super(speedController, encoderPidSrc, maxRotationalRate, kP, kI, kD, maxIntegralError, deadband);
+    }
+
+    /**
+     * Enable traction control
+     * @param tractionEnabled
+     */
+    public void setTractionEnabled(boolean tractionEnabled) {
+        this.tractionEnabled = tractionEnabled;
+    }
+
+    /**
+     * Check if traction control is enabled
+     * @return
+     */
+    public boolean isTractionEnabled() {
+        return tractionEnabled;
+    }
+
+    @Override
+    public void set(final double v, final byte b) {
+        if (tractionEnabled) {
+            super.set(v, b);
+        } else {
+            speedController.set(v, b);
+        }
+    }
+
+    @Override
+    public void set(final double v) {
+        if (tractionEnabled) {
+            super.set(v);
+        } else {
+            speedController.set(v);
+        }
+    }
+}
